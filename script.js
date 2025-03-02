8ec9d49a3f43f717fc098c7bfd91c8cad81871ae
creppergamer456 <creppergamer456@gmail.com>
2025-03-02 15:36:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  var num = 0;
  const width = window.innerWidth;
- var imagen = [document.getElementById("Imagen"), document.getElementById("Imagen1"), document.getElementById("Imagen2"), document.getElementById("Imagen3"), document.getElementById("Imagen4"), document.getElementById("Imagen5"), document.getElementById("Imagen6"), document.getElementById("Logo")];
+ var imagen = [document.getElementById("Imagen"), document.getElementById("Imagen1"), document.getElementById("Imagen2"), document.getElementById("Imagen3"), document.getElementById("Imagen4"), document.getElementById("Imagen5"), document.getElementById("Imagen6"),document.getElementById("Imagen7"), document.getElementById("Logo")];
  var h1 = [document.getElementById("h1_1"), document.getElementById("h1_2"), document.getElementById("h1_3"), document.getElementById("h1_4"), document.getElementById("h1_5"), document.getElementById("h1_6"), document.getElementById("h1_7"), document.getElementById("h1_8")];
  var h2 = [document.getElementById("h2_1"), document.getElementById("h2_2"), document.getElementById("h2_3"), document.getElementById("h2_4"), document.getElementById("h2_5"), document.getElementById("h2_6"), document.getElementById("h2_7"), document.getElementById("h2_8"), document.getElementById("h2_9"), document.getElementById("h2_10")];
  var pos_inicial = 0;
@@ -51,7 +51,8 @@
      imagen[4].style.left = pos_inicial + width * 4 + "px";
      imagen[5].style.left = pos_inicial + width * 5 + "px";
      imagen[6].style.left = pos_inicial + width * 6 + "px";
-     imagen[7].style.left = (pos_inicial + width - 300) + "px";
+     imagen[7].style.left = pos_inicial + width * 6 + "px";
+     imagen[8].style.left = (pos_inicial + width - 300) + "px";
      h1[0].style.left = (pos_inicial + width / 2) + "px";
      h2[0].style.left = (pos_inicial + width / 2) + "px";
      h1[1].style.left = (pos_inicial + width * 2 + width / 2) + "px";
